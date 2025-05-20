1674acc4a32b85e085459647bc0300183ad4e1ed
r2d2c2 <uy6806@naver.com>
2025-05-20 12:26:44 +0900

    update

--- WindowsFormsApp_Task_Runing/Form1.cs
+++ WindowsFormsApp_Task_Runing/Form1.cs
@@ -21,35 +21,30 @@ namespace WindowsFormsApp_Task_Runing
 
         private async void button1_Click(object sender, EventArgs e)
         {
-            if (openFileDialog == null || string.IsNullOrEmpty(openFileDialog.FileName))
-            {
-                MessageBox.Show("먼저 파일을 선택하세요.");
-                return;
-            }
-            await ReadFileAsync(openFileDialog.FileName); //비동기 메서드 호출
+            
+            await ReadFileAsync(); //비동기 메서드 호출
         }
-        private async Task ReadFileAsync(string filePath)
-        {
-            using (StreamReader reader = new StreamReader(filePath))
-            {
-                string content = await reader.ReadToEndAsync(); //비동기 파일 읽기
-                textBox1.Text = content; //읽은 내용 출력
-            }
-        }
-
-        private void button2_Click(object sender, EventArgs e)
+        private async Task ReadFileAsync()
         {
             // 파일 탐색기 대화상자 열기
-            openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "텍스트 파일 (*.txt)|*.txt|모든 파일 (*.*)|*.*"; //파일 형식 필터
-            openFileDialog.Title = "파일 선택"; //대화상자 제목
-            openFileDialog.InitialDirectory = "\"C:\\Users\\김철수\\Documents\\"; //초기 디렉토리
-            // 탐색기 창 열기
-            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            using (openFileDialog = new OpenFileDialog())
             {
-                // 선택한 파일 경로 출력
-                textBox1.Text = openFileDialog.FileName;
+                openFileDialog.Filter = "텍스트 파일 (*.txt)|*.txt|모든 파일 (*.*)|*.*"; //파일 형식 필터
+                openFileDialog.Title = "파일 선택"; //대화상자 제목
+                openFileDialog.InitialDirectory = "\"C:\\Users\\김철수\\Documents\\"; //초기 디렉토리
+                                                                                   // 탐색기 창 열기
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // 선택한 파일 경로 출력
+                    textBox1.Text = openFileDialog.FileName;
+                }
+                using (StreamReader reader = new StreamReader(openFileDialog.FileName))
+                {
+                    string content = await reader.ReadToEndAsync(); //비동기 파일 읽기
+                    textBox1.Text = content; //읽은 내용 출력
+                }
             }
+                
         }
     }
 }
